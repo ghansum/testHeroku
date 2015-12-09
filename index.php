27c2347dfbbbf29ce02a6006c0c20ef6472b62c7
ghansum <ghansumn@gmail.com>
2015-12-09 16:56:54 +0100

    commit 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	  <title>Test de Heroku</title>
+		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<h1> Bonjour! <br> Bienvenu dans mon site ! </h1>
+	</body>
+</html>
